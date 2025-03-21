285346f2184a282c2d3b618dde12b2914c0dbdba
Henrik Rydgård <hrydgard@gmail.com>
2025-03-21 09:18:14 +0100

    Move SSL error codes to the big enum

--- Core/HLE/sceSsl.cpp
+++ Core/HLE/sceSsl.cpp
@@ -18,14 +18,10 @@
 #include "Common/Serialize/Serializer.h"
 #include "Common/Serialize/SerializeFuncs.h"
 #include "Core/HLE/HLE.h"
+#include "Core/HLE/ErrorCodes.h"
 #include "Core/HLE/FunctionWrappers.h"
 #include "Core/MemMap.h"
 #include "Core/HLE/sceSsl.h"
-
-#define ERROR_SSL_NOT_INIT          0x80435001;
-#define ERROR_SSL_ALREADY_INIT      0x80435020;
-#define ERROR_SSL_OUT_OF_MEMORY     0x80435022;
-#define ERROR_SSL_INVALID_PARAMETER 0x804351FE;
 
 bool isSslInit;
 u32 maxMemSize;
